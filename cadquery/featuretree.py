2d1f6a2fe0c10bdc234840b7a7ebd4082acc73b5
Dave Cowden <dave.cowden@gmail.com>
2016-03-25 16:50:15 -0400

    added feature tree

--- cadquery/featuretree.py
+++ cadquery/featuretree.py
@@ -1,5 +1,8 @@
 """
 	Feature Tree-- stores the result of operations 
 	as they occur. each operation results in a node in the tree.
-	
 """
+class FeatureTree(object):
+	"""
+		A tree of operation results within.
+	"""
